c68965ea87fc213e3ec600f376ff84faa39f8965
grands-ana <anapossani@gmail.com>
2021-08-12 10:24:23 -0300

    Correção index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-
 import Page from '../common/Page';
 
 const Home: React.FC = () => {
@@ -12,15 +11,15 @@ const Home: React.FC = () => {
     axios
       .get('https://ana-personal-project.herokuapp.com/meal')
       .then((response) => {
-        setRepositories(response.data);
+        setRepositories(response.data[0].meals);
       });
   }, []);
 
   return (
     <>
-      <Page title="Página Principal">
+      <Page title="Pagina Principal">
         <div className="flex items-center justify-center flex-col h-full overflow-auto">
-          <h1 className="text-xl my-7">What's on your mind?</h1>
+          <h1 className="text-xl my-7">Whats on your mind</h1>
               <div className="conteudo">
             {repositories.map((repo) => (
               <div className="flex items-center justify-center flex-col mt-10" key={repo.idMeal}>
