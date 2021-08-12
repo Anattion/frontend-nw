a4996892938a31ba3970b173f6c4f2a2ecef2c8e
grands-ana <anapossani@gmail.com>
2021-08-12 11:08:01 -0300

    AJustes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,35 @@ import axios from 'axios';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import Page from '../src/common/Page'
-import cors from "cors"
 
+import Page from '../src/common/Page'
 
 const Home: React.FC = () => {
   const [repositories, setRepositories] = useState<any[]>([]);
   const [text, setText] = useState('');
   useEffect(() => {
-    axios
-      .get('https://ana-personal-project.herokuapp.com/meal', cors())
-      .then((response) => {
-        console.log(response.data)
-        setRepositories(response.data);
-      });
+    axios.get('https://ana-personal-project.herokuapp.com/meal').then((response) => {
+      setRepositories(response.data[0].meals);
+    });
   }, []);
 
   return (
     <>
-      <Page title="Pagina Principal">
+      <Page title="Página Principal">
         <div className="flex items-center justify-center flex-col h-full overflow-auto">
-          <h1 className="text-xl my-7">Whats on your mind</h1>
-              <div className="conteudo">
+          <h1 className="text-xl my-7">What's on your mind?</h1>
+          <div className="conteudo">
             {repositories.map((repo) => (
               <div className="flex items-center justify-center flex-col mt-10" key={repo.idMeal}>
                 <h2 className="text-left mt-8">
                   <br /> {repo.strMeal}
                 </h2>
-                <img className=".imagem" src={repo.strMealThumb} alt={repo.strMeal} width="200"></img>
+                <img
+                  className=".imagem"
+                  src={repo.strMealThumb}
+                  alt={repo.strMeal}
+                  width="200"
+                ></img>
                 <p className="conteudo-right">
                   <br /> {repo.strInstructions}
                 </p>
