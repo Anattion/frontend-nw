52f85785daa6b5e320641225555731feb5fe1c47
grands-ana <anapossani@gmail.com>
2021-08-12 07:30:01 -0300

    teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import axios from 'axios';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Page from '../src/common/Page'
 
-export default function Home() {
-  
+
+const Home: React.FC = () => {
+  const [repositories, setRepositories] = useState<any[]>([]);
+  const [text, setText] = useState('');
+  useEffect(() => {
+    axios
+      .get('https://ana-personal-project.herokuapp.com/meal')
+      .then((response) => {
+        setRepositories(response.data);
+      });
+  }, []);
+
   return (
-   <div> 
-     
-    </div>
-  )
-}
+    <>
+      <Page title="Pagina Principal">
+        <div className="flex items-center justify-center flex-col h-full overflow-auto">
+          <h1 className="text-xl my-7">Whats on your mind</h1>
+              <div className="conteudo">
+            {repositories.map((repo) => (
+              <div className="flex items-center justify-center flex-col mt-10" key={repo.idMeal}>
+                <h2 className="text-left mt-8">
+                  <br /> {repo.strMeal}
+                </h2>
+                <img className=".imagem" src={repo.strMealThumb} alt={repo.strMeal} width="200"></img>
+                <p className="conteudo-right">
+                  <br /> {repo.strInstructions}
+                </p>
+                <hr></hr>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Page>
+    </>
+  );
+};
+
+export default Home;
